3a809b0162cccb3c7969042b2cde20661a1b2d21
yunlongdong <yunlongdong@outlook.com>
2022-09-11 21:43:54 +0800

    adjust docs and main frame

--- ui/rio/main_frame.py
+++ ui/rio/main_frame.py
@@ -2,6 +2,7 @@ import wx
 from wx import html, stc
 import numpy as np
 import os.path as osp
+import os
 
 from .urdf_show import urdf_show
 
@@ -19,9 +20,8 @@ class MainFrame ( wx.Frame ):
         self.m_html_start_doc = html.HtmlWindow( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.html.HW_SCROLLBAR_AUTO )
         bSizer1.Add( self.m_html_start_doc, 2, wx.EXPAND|wx.ALL, 2 )
 
-        self.m_text_show = stc.StyledTextCtrl( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
-        self.m_text_show.SetLexer(stc.STC_LEX_XML)
-        bSizer1.Add( self.m_text_show, 4, wx.EXPAND |wx.ALL, 5 )
+        self.m_text_show = html.HtmlWindow( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
+        bSizer1.Add( self.m_text_show, 4, wx.EXPAND |wx.ALL, 2 )
 
         # self.m_button1 = wx.Button( self, wx.ID_ANY, u"MyButton", wx.DefaultPosition, wx.DefaultSize, 0 )
         # bSizer1.Add( self.m_button1, 0, wx.ALL, 5 )
@@ -34,27 +34,13 @@ class MainFrame ( wx.Frame ):
         self.m_menuItem_open_urdf = wx.MenuItem( self.MenuFileOpen, wx.ID_ANY, u"URDF\tCtrl+U", wx.EmptyString, wx.ITEM_NORMAL )
         self.MenuFileOpen.Append( self.m_menuItem_open_urdf )
 
-        self.m_menuItem_open_MDH = wx.MenuItem( self.MenuFileOpen, wx.ID_ANY, u"Modified D-H", wx.EmptyString, wx.ITEM_NORMAL )
-        self.MenuFileOpen.Append( self.m_menuItem_open_MDH )
+        # self.m_menuItem_open_MDH = wx.MenuItem( self.MenuFileOpen, wx.ID_ANY, u"Modified D-H", wx.EmptyString, wx.ITEM_NORMAL )
+        # self.MenuFileOpen.Append( self.m_menuItem_open_MDH )
 
         self.MenuFile.AppendSubMenu( self.MenuFileOpen, u"Open" )
 
         self.MenuMain.Append( self.MenuFile, u"File" )
 
-        self.MenuKinematics = wx.Menu()
-        self.m_menuItem_kin_FK = wx.MenuItem( self.MenuKinematics, wx.ID_ANY, u"Forward Kinematics", wx.EmptyString, wx.ITEM_NORMAL )
-        self.MenuKinematics.Append( self.m_menuItem_kin_FK )
-
-        self.m_menuItem_kin_IK = wx.MenuItem( self.MenuKinematics, wx.ID_ANY, u"Inverse Kinematics", wx.EmptyString, wx.ITEM_NORMAL )
-        self.MenuKinematics.Append( self.m_menuItem_kin_IK )
-
-        self.MenuMain.Append( self.MenuKinematics, u"Kinematics" )
-
-        self.MenuDynamics = wx.Menu()
-        self.MenuMain.Append( self.MenuDynamics, u"Dynamics" )
-
-        self.MenuVis = wx.Menu()
-        self.MenuMain.Append( self.MenuVis, u"Visulization" )
 
         self.SetMenuBar( self.MenuMain )
 
@@ -72,6 +58,7 @@ class MainFrame ( wx.Frame ):
 
         # start doc
         self.m_html_start_doc.LoadFile(osp.join(dir_abs_path, "../../docs/start.html"))
+        self.m_text_show.LoadFile(osp.join(dir_abs_path, "../../docs/support_us.html"))
 
         # bind event
         self.bind_all()
@@ -84,7 +71,8 @@ class MainFrame ( wx.Frame ):
 
     def OnOpenURDF(self, evt):
         print('Open URDF...')
-        with wx.FileDialog(self, "Open URDF file", wildcard="URDF files (*.urdf)|*.urdf",
+        print(osp.dirname(osp.abspath(__file__)))
+        with wx.FileDialog(self, "Open URDF file", defaultDir=osp.join(dir_abs_path, '../../urdf_examples/kuka iiwa') ,wildcard="URDF files (*.urdf)|*.urdf",
                        style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST) as fileDialog:
 
             if fileDialog.ShowModal() == wx.ID_CANCEL:
@@ -93,7 +81,7 @@ class MainFrame ( wx.Frame ):
             # Proceed loading the file chosen by the user
             pathname = fileDialog.GetPath()
             
-            self.m_text_show.ChangeValue(open(pathname, 'r').read())
+            
             self.m_statusBar.SetStatusText(pathname)
             print(pathname)
             self.pop_3d_viewer(pathname)
